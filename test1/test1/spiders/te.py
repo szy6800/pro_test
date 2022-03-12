e6cd1bfcf5888468d0f2946dddf6b451a4ab1512
szy <szy@qq.com>
2022-03-12 12:15:40 +0800

    测试提交

---
+++ test1/test1/spiders/te.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class TeSpider(scrapy.Spider):
+    name = 'te'
+    allowed_domains = ['baidu.com']
+    start_urls = ['http://baidu.com/']
+
+    def parse(self, response):
+        print(response.text)
