a738ee3a84399ccf98114d0f27197584d1bcb768
e <szyyyds@qq.com>
2022-03-21 22:31:26 +0800

    黑马logging 讲解

--- test1/test1/spiders/te.py
+++ test1/test1/spiders/te.py
@@ -1,22 +1,24 @@
 import scrapy
 from scrapy.http import HtmlResponse
 import jsonpath,json
+# https://www.bilibili.com/video/BV1KJ41127MX?p=5
 class TeSpider(scrapy.Spider):
     name = 'te'
     allowed_domains = ['baidu.com']
     start_urls = ["https://www.cnblogs.com/leijiangtao/p/4806683.html"]
 
     def parse(self, response):
-        # 将我们得到的数据封装到一个 `ItcastItem` 对象
-        item = dict()
-        # xpath返回的是包含一个元素的列表
-        item['name'] = '1'
-        item['title'] = '3'
-        item['info'] = '2'
-
-        # 直接返回最后数据
-        # print(items)
-        return item
+        print(response.text)
+        # # 将我们得到的数据封装到一个 `ItcastItem` 对象
+        # item = dict()
+        # # xpath返回的是包含一个元素的列表
+        # item['name'] = '1'
+        # item['title'] = '3'
+        # item['info'] = '2'
+        #
+        # # 直接返回最后数据
+        # # print(items)
+        # return item
 
 
 
