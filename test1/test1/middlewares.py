9a5e4d7b7543b3eb54a196f8c61c75b6acbb88db
szy <szy@qq.com>
2022-03-25 11:42:54 +0800

    selenium 对接scrapy

--- test1/test1/middlewares.py
+++ test1/test1/middlewares.py
@@ -13,7 +13,7 @@ import requests
 from scrapy.http import HtmlResponse
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
-
+from scrapy.http import HtmlResponse
 
 class Test1SpiderMiddleware:
     # Not all methods need to be defined. If a method is not defined,
@@ -88,12 +88,12 @@ class Test1DownloaderMiddleware:
 
     def process_response(self, request, response, spider):
         # Called with the response returned from the downloader.
+        driver = spider.driver
+        driver.get(request.url)
+        text = driver.page_source
 
-        # Must either;
-        # - return a Response object
-        # - return a Request object
-        # - or raise IgnoreRequest
-        return response
+        return HtmlResponse(url=request.url, body=text, request=request, status=200,
+                            encoding='utf-8')
 
     def process_exception(self, request, exception, spider):
         # Called when a download handler or a process_request()
@@ -107,50 +107,50 @@ class Test1DownloaderMiddleware:
 
     def spider_opened(self, spider):
         spider.logger.info('Spider opened: %s' % spider.name)
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
-class SeleniumMiddleware(object):
-    def process_request(self,request,spider):
-        chrome_options = Options()
-        chrome_options.add_argument('--no-sandbox')  # 解决DevToolsActivePort文件不存在的报错
-        # chrome_options.add_argument("--headless")
-        chrome_options.headless = True
-        chrome_options.add_argument('window-size=1400x1015')  # 指定浏览器分辨率
-        chrome_options.add_argument('--disable-gpu')  # 谷歌文档提到需要加上这个属性来规避bug
-        chrome_options.add_argument('--hide-scrollbars')  # 隐藏滚动条, 应对一些特殊页面
-        chrome_options.add_argument('blink-settings=imagesEnabled=false')  # 不加载图片, 提升速度
-        chrome_options.add_argument('--headless')  # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
-        chrome_options.add_argument('--disable-gpu')  # 禁用GPU加速
-        chrome_options.add_argument('--start-maximized')  # 浏览器最大化
-        chrome_options.add_argument('--window-size=1280x1024')  # 设置浏览器分辨率（窗口大小）
-        chrome_options.add_argument('log-level=3')
-        chrome_options.add_argument('--disable-infobars')  # 禁用浏览器正在被自动化程序控制的提示
-        chrome_options.add_argument('--incognito')  # 隐身模式（无痕模式）
-        chrome_options.add_argument('--disable-javascript')  # 禁用javascript
-        chrome_options.add_argument('--ignore-certificate-errors')  # 禁用扩展插件并实现窗口最大化
-        chrome_options.add_argument('–disable-software-rasterizer')
-        chrome_options.add_argument('--disable-extensions')
-        chrome_options.add_argument(f'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit'
-                                    f'/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36')
-        chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])
-        chrome_options.add_experimental_option('useAutomationExtension', False)
-        browser = webdriver.Chrome(options=chrome_options)
-        browser.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
-            "source": """
-            Object.defineProperty(navigator, 'webdriver', {
-              get: () => undefined
-            })
-          """
-        })
-        #driver.maximize_window()
-        browser.get(request.url)
-        time.sleep(3)
-        text = browser.page_source
-        # print(text)
-        browser.quit()
-        # https://doc.scrapy.org/en/latest/topics/request-response.html
-        # 查看 HtmlResponse 对象结构
-        return HtmlResponse(url=request.url,encoding='utf-8',body=text,request=request)
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
+#
+# class SeleniumMiddleware(object):
+#     def process_request(self,request,spider):
+#         chrome_options = Options()
+#         chrome_options.add_argument('--no-sandbox')  # 解决DevToolsActivePort文件不存在的报错
+#         # chrome_options.add_argument("--headless")
+#         chrome_options.headless = True
+#         chrome_options.add_argument('window-size=1400x1015')  # 指定浏览器分辨率
+#         chrome_options.add_argument('--disable-gpu')  # 谷歌文档提到需要加上这个属性来规避bug
+#         chrome_options.add_argument('--hide-scrollbars')  # 隐藏滚动条, 应对一些特殊页面
+#         chrome_options.add_argument('blink-settings=imagesEnabled=false')  # 不加载图片, 提升速度
+#         chrome_options.add_argument('--headless')  # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
+#         chrome_options.add_argument('--disable-gpu')  # 禁用GPU加速
+#         chrome_options.add_argument('--start-maximized')  # 浏览器最大化
+#         chrome_options.add_argument('--window-size=1280x1024')  # 设置浏览器分辨率（窗口大小）
+#         chrome_options.add_argument('log-level=3')
+#         chrome_options.add_argument('--disable-infobars')  # 禁用浏览器正在被自动化程序控制的提示
+#         chrome_options.add_argument('--incognito')  # 隐身模式（无痕模式）
+#         chrome_options.add_argument('--disable-javascript')  # 禁用javascript
+#         chrome_options.add_argument('--ignore-certificate-errors')  # 禁用扩展插件并实现窗口最大化
+#         chrome_options.add_argument('–disable-software-rasterizer')
+#         chrome_options.add_argument('--disable-extensions')
+#         chrome_options.add_argument(f'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit'
+#                                     f'/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36')
+#         chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])
+#         chrome_options.add_experimental_option('useAutomationExtension', False)
+#         browser = webdriver.Chrome(options=chrome_options)
+#         browser.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+#             "source": """
+#             Object.defineProperty(navigator, 'webdriver', {
+#               get: () => undefined
+#             })
+#           """
+#         })
+#         #driver.maximize_window()
+#         browser.get(request.url)
+#         time.sleep(3)
+#         text = browser.page_source
+#         # print(text)
+#         browser.quit()
+#         # https://doc.scrapy.org/en/latest/topics/request-response.html
+#         # 查看 HtmlResponse 对象结构
+#         return HtmlResponse(url=request.url,encoding='utf-8',body=text,request=request)
 
 
--- test1/test1/spiders/te.py
+++ test1/test1/spiders/te.py
@@ -1,15 +1,28 @@
 import scrapy
 from scrapy.http import HtmlResponse
 import jsonpath,json
+from selenium import webdriver
 # https://www.bilibili.com/video/BV1KJ41127MX?p=5
 #访问
 class TeSpider(scrapy.Spider):
     name = 'te'
-    allowed_domains = ['baidu.com']
-    start_urls = ["https://www.cnblogs.com/leijiangtao/p/4806683.html"]
+    start_urls = ["https://www.ebidding.com/portal/html/index.html#page=main:announcement"]
+    # allowed_domains = ['baidu.com']
+
+    def __init__(self):
+        super(TeSpider, self).__init__()
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless')
+        self.driver = webdriver.Chrome(options=options)
+
+    def close(self, spider):
+        self.driver.quit()
+        print('selenium 关闭')
+
 
     def parse(self, response):
-        print(response.text)
+        titles = response.xpath('//*[@id="a_head"]/li[position()>1]//*[@class="a_title"]/@title').getall()
+        print(titles)
         # # 将我们得到的数据封装到一个 `ItcastItem` 对象
         # item = dict()
         # # xpath返回的是包含一个元素的列表
